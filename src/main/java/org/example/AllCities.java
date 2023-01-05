cbdca7579682d3f793ff23a2e9fee1aa082ea36a
max5045955@gmail.com <max5045955@gmail.com>
2023-01-05 17:55:23 +0300

    changes methods and add some comments

--- src/main/java/org/example/AllCities.java
+++ src/main/java/org/example/AllCities.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,52 +6,69 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.example.LoginAndPassword.LGN;
-
 public class AllCities {
     public static void main(String[] args) {
+        //driver launch
         System.setProperty("webdriver.chrome.driver", "F:\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
+        // open login page, and login
         driver.get("https://visa.vfsglobal.com/blr/ru/pol/login");
         new WebDriverWait(driver, 15).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("mat-input-0")));
-        driver.findElement(By.id("mat-input-0")).sendKeys(LGN);
+        driver.findElement(By.id("mat-input-0")).sendKeys(LoginAndPassword.LGN);
         driver.findElement(By.id("mat-input-1")).sendKeys(LoginAndPassword.PSWD);
+
+        //close cookie's window
         driver.findElement(By.xpath("//*[@id=\"onetrust-close-btn-container\"]/button")).click();
-        waitingDriver(5000L);
+        timer(5000L);
+
+        //c
+        // lick Login button
         driver.findElement(By.xpath("//button[contains(@class,'ng-star-inserted')]")).click();
-        waitingDriver(15000L);
+        timer(15000L);
+
+        //click booking button
         driver.findElement(By.xpath("//button[contains(@class,'z-index-999')]")).click();
-        waitingDriver(10000L);
+        timer(10000L);
+
+        //array of cities
         String[] cities = {"Grodno", "Baranovichi", "Brest", "Gomel", "Lida", "Minsk", "Mogilev", "Pinsk"};
 
+        //replace element of cities
         for (int i = 0; i < cities.length; i++) {
-            waitingDriver(2000L);
+            timer(2000L);
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-1\"]/span")).click();
             System.out.println("Визовый центр " + cities[i]);
-            waitingDriver(2000L);
+            timer(2000L);
             driver.findElement(By.xpath("//span[contains(text(),'Poland Visa Application Center-" + cities[i] + "')]")).click();
-            timeCheckOut(driver);
+            changeVisaType(driver);
         }
+        //driver ends
         driver.close();
     }
-    public static void waitingDriver(Long number) {
+
+    //a method that pauses the driver
+    public static void timer(Long number) {
         try {
             Thread.sleep(number);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    public static void timeCheckOut(WebDriver driver) {
-        waitingDriver(10000L);
-        driver.findElement(By.xpath("//*[@id=\"mat-select-value-3\"]/span")).click();
-        waitingDriver(2000L);
 
+    // select category and visa type
+    public static void changeVisaType(WebDriver driver) {
+        timer(10000L);
+        driver.findElement(By.xpath("//*[@id=\"mat-select-value-3\"]/span")).click();
+        timer(2000L);
         driver.findElement(By.xpath("//span[contains(text(), ' National Visa D ')]")).click();
-        waitingDriver(10000L);
+        timer(10000L);
         driver.findElement(By.xpath("//*[@id=\"mat-select-value-5\"]/span")).click();
-        waitingDriver(2000L);
+        timer(2000L);
         driver.findElement(By.xpath("//span[contains(text(),' Karta Polaka D-visa ')]")).click();
-        waitingDriver(10000L);
+        timer(10000L);
+
+        // print text result
         WebElement webElement = driver.findElement(By.xpath("//div[contains(@class,'alert-info')]"));
         String textElement = webElement.getText();
         System.out.println(textElement + "\n");
