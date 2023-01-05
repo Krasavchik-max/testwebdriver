53ad63c2fc6d994801fb45e35ad54158ac725d82
max5045955@gmail.com <max5045955@gmail.com>
2023-01-05 11:17:07 +0300

    fix bug with timeCheckOut and add Date

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,7 +19,7 @@ public class Main {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://visa.vfsglobal.com/blr/ru/pol/login");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("mat-input-0")));
+        new WebDriverWait(driver, 15).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("mat-input-0")));
         driver.findElement(By.id("mat-input-0")).sendKeys(LGN);
         driver.findElement(By.id("mat-input-1")).sendKeys(LoginAndPassword.PSWD);
         driver.findElement(By.xpath("//button[contains(@class,'ng-star-inserted')]")).click();
@@ -26,7 +27,7 @@ public class Main {
 
 
 
-        waitingDriver(10000L);
+        waitingDriver(15000L);
         System.out.println("После таймаута");
         driver.findElement(By.xpath("//button[contains(@class,'z-index-999')]")).click();
 
@@ -37,7 +38,7 @@ public class Main {
 
 
 
-        driver.findElement(By.xpath("//span[contains(@class,'ng-tns-c85-4')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'Выберите свой визовый центр')]")).click();
         System.out.println("Визовый центр ");
         driver.findElement(By.xpath("//span[contains(text(),'Poland Visa Application Center-Grodno')]")).click();
 
@@ -45,9 +46,9 @@ public class Main {
         waitingDriver(10000L);
 
 
-        driver.findElement(By.xpath("//span[contains(@class,'ng-tns-c85-6')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'Выберите категорию записи')]")).click();
         System.out.println("Категория Записи");
-        driver.findElement(By.xpath("//span[contains(text(),'National Visa D')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(), ' National Visa D ')]")).click();
 
         waitingDriver(10000L);
 
@@ -71,20 +72,25 @@ public class Main {
         }
     }
    public static void timeCheckOut(WebDriver driver){
+       waitingDriver(10000L);
        driver.findElement(By.xpath("//span[contains(@class,'ng-tns-c85-8')]")).click();
-       waitingDriver(3000L);
-       driver.findElement(By.xpath("//span[contains(text(),' Other D-visa ')]")).click();
+       waitingDriver(2000L);
+       driver.findElement(By.xpath("//span[contains(text(),'Other D-visa')]")).click();
 
        waitingDriver(10000L);
        driver.findElement(By.xpath("//span[contains(@class,'ng-tns-c85-8')]")).click();
-       waitingDriver(3000L);
+       waitingDriver(2000L);
        driver.findElement(By.xpath("//span[contains(text(),' Karta Polaka D-visa ')]")).click();
 
        waitingDriver(10000L);
 
        WebElement webElement = driver.findElement(By.xpath("//div[contains(@class,'alert-info')]"));
        String textElement = webElement.getText();
+
+       Date date = new Date();
        System.out.println(textElement);
+       System.out.print(date);
+
 
 
    }
