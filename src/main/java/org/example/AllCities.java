2e126653edab22fd55388b057e3fe2886c9da0ac
Viktar Krauchyk <viktar.krauchyk@thomsonreuters.com>
2023-01-09 15:33:58 +0100

    replace timer to toTimeUnit sleep add time stamp refactor method name getDatesFromAllCities

--- src/main/java/org/example/AllCities.java
+++ src/main/java/org/example/AllCities.java
@@ -6,8 +6,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 public class AllCities {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //driver launch
         System.setProperty("webdriver.chrome.driver", "F:\\chromedriver.exe");
@@ -15,63 +19,54 @@ public class AllCities {
 
         // open login page, and login
         driver.get("https://visa.vfsglobal.com/blr/ru/pol/login");
-        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("mat-input-0")));
+        new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("mat-input-0")));
         driver.findElement(By.id("mat-input-0")).sendKeys(LoginAndPassword.LGN);
         driver.findElement(By.id("mat-input-1")).sendKeys(LoginAndPassword.PSWD);
-        timer(5000L);
+        TimeUnit.SECONDS.sleep(5);
 
         //close cookie's window
         driver.findElement(By.xpath("//*[@id=\"onetrust-close-btn-container\"]/button")).click();
 
         // click Login button
         driver.findElement(By.xpath("//button[contains(@class,'ng-star-inserted')]")).click();
-        timer(15000L);
+        TimeUnit.SECONDS.sleep(15);
 
         //click booking button
         driver.findElement(By.xpath("//button[contains(@class,'z-index-999')]")).click();
-        timer(10000L);
+        TimeUnit.SECONDS.sleep(10);
 
         for (int i = 0; i < 100; i++) {
-            changeCity(driver);
-            timer(480000L);
-
-        }
-    }
-
-
-    //a method that pauses the driver
-    public static void timer(Long number) {
-        try {
-            Thread.sleep(number);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println(DateTimeFormatter.ofPattern("dd MMM YYYY, hh:mm:ss a").format(LocalDateTime.now()));
+            getDatesFromAllCities(driver);
+            TimeUnit.MINUTES.sleep(14);
         }
     }
 
     // select category and visa type
-    public static void changeCity(WebDriver driver) {
+    public static void getDatesFromAllCities(WebDriver driver) throws InterruptedException {
 
-        //select city from array
-        String[] cities = {"Grodno", "Baranovichi", "Brest", "Gomel", "Lida", "Minsk", "Mogilev", "Pinsk"};
+        //city array
+        String[] cities = {"Grodno", "Lida", "Minsk", "Baranovichi", "Brest", "Pinsk", "Gomel", "Mogilev"};
 
-        //replace element of cities
         for (int i = 0; i < cities.length; i++) {
-            timer(5000L);
+            //select city
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-1\"]/span")).click();
-            System.out.println("Визовый центр " + cities[i]);
-            timer(5000L);
+            System.out.println(cities[i]);
+            TimeUnit.SECONDS.sleep(5);
             driver.findElement(By.xpath("//span[contains(text(),'Poland Visa Application Center-" + cities[i] + "')]")).click();
+            TimeUnit.SECONDS.sleep(10);
 
-        // select type and kind of visa
-            timer(10000L);
+            // select kind of visa
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-3\"]/span")).click();
-            timer(2000L);
+            TimeUnit.SECONDS.sleep(2);
             driver.findElement(By.xpath("//span[contains(text(), ' National Visa D ')]")).click();
-            timer(10000L);
+            TimeUnit.SECONDS.sleep(10);
+
+            // select type of visa
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-5\"]/span")).click();
-            timer(2000L);
+            TimeUnit.SECONDS.sleep(2);
             driver.findElement(By.xpath("//span[contains(text(),' Karta Polaka D-visa ')]")).click();
-            timer(10000L);
+            TimeUnit.SECONDS.sleep(10);
 
             // print text result
             String textElement = driver.findElement(By.xpath("//div[contains(@class,'alert-info')]")).getText();
@@ -79,8 +74,3 @@ public class AllCities {
         }
     }
 }
-
-
-
-
-
