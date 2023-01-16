5b25ebf3e9b605c292ae825d9f3c5f9ea91157c6
max5045955@gmail.com <max5045955@gmail.com>
2023-01-17 01:03:39 +0300

    initial commit

--- src/main/java/org/example/AllCities.java
+++ src/main/java/org/example/AllCities.java
@@ -9,10 +9,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
 import static org.example.ConnectionToDB.*;
 
 public class AllCities {
-    public static void main(String[] args)  throws SQLException {
+    public static void main(String[] args) throws InterruptedException {
 
         //driver launch
         System.setProperty("webdriver.chrome.driver", "F:\\chromedriver.exe");
@@ -23,59 +24,57 @@ public class AllCities {
         new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("mat-input-0")));
         driver.findElement(By.id("mat-input-0")).sendKeys(LoginAndPassword.LGN);
         driver.findElement(By.id("mat-input-1")).sendKeys(LoginAndPassword.PSWD);
-        timer(5000L);
+        TimeUnit.SECONDS.sleep(15);
 
         //close cookie's window
         driver.findElement(By.xpath("//*[@id=\"onetrust-close-btn-container\"]/button")).click();
 
         // click Login button
         driver.findElement(By.xpath("//button[contains(@class,'ng-star-inserted')]")).click();
-        timer(15000L);
+        TimeUnit.SECONDS.sleep(5);
 
         //click booking button
         driver.findElement(By.xpath("/html/body/app-root/div/app-dashboard/section[1]/div/div[1]/div[2]/button/span[1]")).click();
-        timer(10000L);
+        TimeUnit.SECONDS.sleep(5);
 
         for (int i = 0; i < 100; i++) {
-            changeCity(driver);
+            try {
+                getDatesFromAllCities(driver);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println("------------------------------------------------------------------------");
-            timer(480000L);
+            TimeUnit.SECONDS.sleep(15);
 
         }
     }
 
 
     //a method that pauses the driver
-    public static void timer(Long number) {
-        try {
-            Thread.sleep(number);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+
 
     // select category and visa type
-    public static void changeCity(WebDriver driver) {
+    public static void getDatesFromAllCities(WebDriver driver) throws InterruptedException {
 
         //select city from array
-        String[] cities = {"Baranovichi", "Brest", "Gomel", "Grodno", "Lida", "Minsk", "Mogilev", "Pinsk"};
+        String[] cities = {"Grodno", "Lida", "Minsk", "Baranovichi", "Brest", "Pinsk", "Gomel", "Mogilev"};
 
         //replace element of cities
         for (int i = 0; i < cities.length; i++) {
-            timer(5000L);
+            TimeUnit.SECONDS.sleep(15);
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-1\"]/span")).click();
             System.out.println("Визовый центр " + cities[i]);
-            timer(2000L);
+            TimeUnit.SECONDS.sleep(2);
             driver.findElement(By.xpath("//span[contains(text(),'Poland Visa Application Center-" + cities[i] + "')]")).click();
 
-        // select type and kind of visa
-            timer(10000L);
+            // select type and kind of visa
+            TimeUnit.SECONDS.sleep(10);
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-3\"]/span")).click();
             driver.findElement(By.xpath("//span[contains(text(), ' National Visa D ')]")).click();
-            timer(10000L);
+            TimeUnit.SECONDS.sleep(10);
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-5\"]/span")).click();
             driver.findElement(By.xpath("//span[contains(text(),' Karta Polaka D-visa ')]")).click();
-            timer(7000L);
+            TimeUnit.SECONDS.sleep(10);
 
             // print text result
             String textElement = driver.findElement(By.xpath("//div[contains(@class,'alert-info')]")).getText();
@@ -84,18 +83,20 @@ public class AllCities {
 
             //connect to DB and send request
             try {
-                Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
+                Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 Statement statement = connection.createStatement();
-                statement.execute("insert into visacenter (city,message,date_time) values ('"+ cities[i] +"','"+textElement+"',NOW())");
+                statement.execute("insert into visacenter (city,message,date_time) values ('" + cities[i] + "','" + textElement + "',NOW())");
                 System.out.println("Add to DB successfully");
 
-            }  catch (SQLException e){
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
 
-        }
+
+
     }
+}
 
 
 
