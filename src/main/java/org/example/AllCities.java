ada628894f09ef605529e9f5723d872159c45a2c
Viktar Krauchyk <viktar.krauchyk@thomsonreuters.com>
2023-01-18 00:30:47 +0100

    Add Sound playing if Grodno has dates Added database 100 iterations with 2 getData

--- src/main/java/org/example/AllCities.java
+++ src/main/java/org/example/AllCities.java
@@ -5,98 +5,108 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import static org.example.ConnectionToDB.*;
+
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 public class AllCities {
-    public static void main(String[] args)  throws SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException, IOException {
 
-        //driver launch
-        System.setProperty("webdriver.chrome.driver", "F:\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        // Setup 100 iterations of log in log out
+        for (int i = 1; i < 100; i++) {
+            //driver launch
+//        System.setProperty("webdriver.chrome.driver", "F:\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
 
-        // open login page, and login
-        driver.get("https://visa.vfsglobal.com/blr/ru/pol/login");
-        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("mat-input-0")));
-        driver.findElement(By.id("mat-input-0")).sendKeys(LoginAndPassword.LGN);
-        driver.findElement(By.id("mat-input-1")).sendKeys(LoginAndPassword.PSWD);
-        timer(5000L);
+            // open login page, and login
+            driver.get("https://visa.vfsglobal.com/blr/ru/pol/login");
+            new WebDriverWait(driver, 60).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("mat-input-0")));
+            driver.findElement(By.id("mat-input-0")).sendKeys(LoginAndPassword.LGN);
+            driver.findElement(By.id("mat-input-1")).sendKeys(LoginAndPassword.PSWD);
+            TimeUnit.SECONDS.sleep(5);
 
-        //close cookie's window
-        driver.findElement(By.xpath("//*[@id=\"onetrust-close-btn-container\"]/button")).click();
+            //close cookie's window
+            driver.findElement(By.xpath("//*[@id=\"onetrust-close-btn-container\"]/button")).click();
 
-        // click Login button
-        driver.findElement(By.xpath("//button[contains(@class,'ng-star-inserted')]")).click();
-        timer(15000L);
+            // click Login button
+            driver.findElement(By.xpath("//button[contains(@class,'ng-star-inserted')]")).click();
+            TimeUnit.SECONDS.sleep(15);
 
-        //click booking button
-        driver.findElement(By.xpath("/html/body/app-root/div/app-dashboard/section[1]/div/div[1]/div[2]/button/span[1]")).click();
-        timer(10000L);
+            //click booking button
+            driver.findElement(By.xpath("//button[contains(@class,'z-index-999')]")).click();
+            TimeUnit.SECONDS.sleep(10);
 
-        for (int i = 0; i < 100; i++) {
-            changeCity(driver);
-            System.out.println("------------------------------------------------------------------------");
-            timer(480000L);
-
-        }
-    }
-
-
-    //a method that pauses the driver
-    public static void timer(Long number) {
-        try {
-            Thread.sleep(number);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            // Setup 2 iterations of get dates
+            for (int j = 0; j < 2; j++) {
+                System.out.println(DateTimeFormatter.ofPattern("dd MMM yyyy, hh:mm:ss a").format(LocalDateTime.now()));
+                getDatesFromAllCities(driver);
+                TimeUnit.MINUTES.sleep(14);
+            }
+            driver.close();
         }
     }
 
     // select category and visa type
-    public static void changeCity(WebDriver driver) {
+    public static void getDatesFromAllCities(WebDriver driver) throws InterruptedException, UnsupportedAudioFileException, LineUnavailableException, IOException {
 
-        //select city from array
-        String[] cities = {"Baranovichi", "Brest", "Gomel", "Grodno", "Lida", "Minsk", "Mogilev", "Pinsk"};
+        //city array
+        String[] cities = {"Grodno", "Lida", "Minsk", "Baranovichi", "Brest", "Pinsk", "Gomel", "Mogilev"};
 
-        //replace element of cities
         for (int i = 0; i < cities.length; i++) {
-            timer(5000L);
+            //select city
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-1\"]/span")).click();
-            System.out.println("Визовый центр " + cities[i]);
-            timer(2000L);
+            System.out.println(cities[i]);
+            TimeUnit.SECONDS.sleep(2);
             driver.findElement(By.xpath("//span[contains(text(),'Poland Visa Application Center-" + cities[i] + "')]")).click();
+            TimeUnit.SECONDS.sleep(5);
 
-        // select type and kind of visa
-            timer(10000L);
+            // select kind of visa
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-3\"]/span")).click();
+            TimeUnit.SECONDS.sleep(2);
             driver.findElement(By.xpath("//span[contains(text(), ' National Visa D ')]")).click();
-            timer(10000L);
+            TimeUnit.SECONDS.sleep(5);
+
+            // select type of visa
             driver.findElement(By.xpath("//*[@id=\"mat-select-value-5\"]/span")).click();
+            TimeUnit.SECONDS.sleep(2);
             driver.findElement(By.xpath("//span[contains(text(),' Karta Polaka D-visa ')]")).click();
-            timer(7000L);
+            TimeUnit.SECONDS.sleep(5);
 
             // print text result
             String textElement = driver.findElement(By.xpath("//div[contains(@class,'alert-info')]")).getText();
-            System.out.println(textElement + "\n");
+            System.out.println(textElement);
 
 
             //connect to DB and send request
             try {
-                Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
+                //CREATE TABLE visacenter(id INT auto_increment primary key, date DATE not null,time TIME not null, city VARCHAR(15) not null, message VARCHAR(80) not null)
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection connection = DriverManager.getConnection(LoginAndPassword.URL, LoginAndPassword.USER, LoginAndPassword.PASSWORD);
                 Statement statement = connection.createStatement();
-                statement.execute("insert into visacenter (city,message,date_time) values ('"+ cities[i] +"','"+textElement+"',NOW())");
-                System.out.println("Add to DB successfully");
+                statement.execute("insert into visacenter (city,message,date,time) values ('" + cities[i] + "','" + textElement + "',current_date(),current_timestamp())");
+                System.out.println("Add to DB successfully" + "\n");
 
-            }  catch (SQLException e){
+            } catch (SQLException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
-        }
-
+            if (cities[i].equals("Grodno") && (!textElement.equals("В настоящее время нет свободных мест для записи") &&
+                    !textElement.equals("Произошла ошибка. Пожалуйста, попробуйте еще раз через некоторое время."))) {
+                PlayAudio.main();
+                System.out.println("ЕСТЬ ДАТЫ !!!");
+            }
         }
     }
-
+}
 
 
 
