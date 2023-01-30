385485c69f888c738f5a5cfd4c8cc9a3e377337a
Viktar Krauchyk <viktar.krauchyk@thomsonreuters.com>
2023-01-30 23:42:01 +0100

    Add sending Email

--- src/main/java/org/example/SendEmail.java
+++ src/main/java/org/example/SendEmail.java
@@ -1,62 +1,41 @@
 package org.example;
+
+
 import javax.mail.*;
 import java.util.Properties;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-import static org.example.AllCities.cities;
-import static org.example.AllCities.textElement;
-
 public class SendEmail {
 
     public static void main(String[] args){
-        sendEmail();
+
+        sendEmail("Тема","Сообщение");
     }
-    public static void sendEmail(){
-        String username = "put your email";
-        String password = "put your password";
+    public static void sendEmail(String emailSubject,String emailBody){
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.ssl.trust", "*");
-        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.auth", "true");
-
-
 
         Session session = Session.getInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
+                        return new PasswordAuthentication(LoginAndPassword.emailFrom, LoginAndPassword.emailPassword);
                     }
                 });
 
         try {
-
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("put a sender email "));
-            message.setSubject("NEW NOTIFICATION ");
-            message.setText("Dear User,"+ textElement +"\n" + cities[6].toUpperCase() +  "\n"
-                    + "ДАТЫ ЕСТЬ!!!!!");
-
-            try {
-                Transport.send(message);
-                System.out.println("Done");
-            } catch (MessagingException e) {
+            message.setFrom(new InternetAddress(LoginAndPassword.emailPassword));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(LoginAndPassword.emailTo));
+            message.setSubject(emailSubject);
+            message.setText(emailBody);
+            Transport.send(message);
+        } catch (MessagingException e) {
                 e.printStackTrace();
             }
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-
     }
-
-
 }
